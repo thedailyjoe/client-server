ebc6d7863de817ca3345f79cb56d27134ff28670
Tyler Long <blackbelt2817@yahoo.com>
2019-08-09 12:52:18 -0400

    Add mongo connection

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const db = require('./api/util/keys').mongoURI;
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+mongoose.set('useCreateIndex', true);
+
 app.get('/', function(req, res) {
     res.json({message: "config up and running"});
 });
 
+app.get('/cart', function (req, res) {
+    res.json({ message: "config up and running" });
+});
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log('working just fine');
 });
 
